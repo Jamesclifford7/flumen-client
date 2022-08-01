bc49318585248c1bfa8aaad3a5da2c7c4cc3aadc
James Clifford <jamesclifford7@gmail.com>
2022-08-01 16:14:32 -0700

    added basic styling to employer dashboard

--- src/components/Employers/Dashboard/index.tsx
+++ src/components/Employers/Dashboard/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import NavBar from '../../NavBar';
+import './index.css'
 
-export default function EmployerDashboard(props: {handleLogout: () => void}) {
+export default function EmployerDashboard(props: {user: any; handleLogout: () => void}) {
+    const {email} = props.user
     return (
         <>
             <NavBar handleLogout={props.handleLogout} />
-            <h1>This is where the Employer dashboard will go</h1>
+            <div className='dashboard'>
+                <h2>Welcome back, {email}!</h2>
+                <h4>My Challenges:</h4>
+            </div>
         </>
     )
 }; 
